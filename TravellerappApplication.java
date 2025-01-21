a2a94390e4ff807ddfae37393515ac3dfd745989
Harshadabh <105583231+Harshadabh@users.noreply.github.com>
2025-01-21 12:08:20 +0530

    Add files via upload

---
+++ TravellerappApplication.java
@@ -0,0 +1,13 @@
+package edu.jsp.travellerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellerappApplication.class, args);
+	}
+
+}
